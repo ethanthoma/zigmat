695a5fde322f68805ec798224d1af1346e9e0c86
Ethan Thoma <ethanthoma@gmail.com>
2023-12-12 17:48:31 -0800

    improved matrix multiplication code and readability

--- src/main.zig
+++ src/main.zig
@@ -18,49 +18,21 @@ fn prettyPrintTime(allocator: std.mem.Allocator, duration: u64) ![]const u8 {
     }
 }
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+pub fn main() !void {}
 
-    // define mat
-    const n: usize = 4;
+test "thread pool" {
+    const thread_pool_options = std.Thread.Pool.Options{ .allocator = std.testing.allocator };
+    var thread_pool: std.Thread.Pool = undefined;
 
-    var mat = try Matrix.init(allocator, n, n);
-    defer mat.deinit();
+    try thread_pool.init(thread_pool_options);
+    defer thread_pool.deinit();
 
-    var count: f32 = 1;
-    for (0..n) |i| {
-        for (0..n) |j| {
-            mat.setValue(i, j, count);
-            count += 1;
-        }
+    const n_jobs = 1000000;
+    for (0..n_jobs) |i| {
+        try thread_pool.spawn(threadFunction, .{i});
     }
+}
 
-    // define mat2
-    var mat2 = try Matrix.identity(allocator, n, n);
-    defer mat2.deinit();
-    for (0..n) |i| {
-        for (0..n) |j| {
-            mat2.setValue(i, j, 1);
-        }
-    }
-
-    // time gemm
-    var timer = try std.time.Timer.start();
-    const result = try mat.matmul(&mat2);
-    const duration = timer.read();
-    const time = try prettyPrintTime(allocator, duration);
-    std.debug.print("Matrix multiplication of two {}x{} matrices took {s}.\n", .{ n, n, time });
-
-    // assert gemm
-    const tol = std.math.floatEps(f32) * (n + 1);
-    for (0..n) |i| {
-        var sum_of_row: f32 = 0;
-        for (0..n) |j| {
-            sum_of_row += mat.data[i * n + j];
-        }
-        for (0..n) |j| {
-            try std.testing.expectApproxEqRel(sum_of_row, result.data[i * n + j], tol);
-        }
-    }
+fn threadFunction(arg: usize) void {
+    std.debug.print("Thread: {}\n", .{arg});
 }
--- src/matrix.zig
+++ src/matrix.zig
@@ -6,9 +6,11 @@ test {
 
 const MatrixError = error{
     NonMatchingDims,
+    InvalidShape,
 };
 
 const block_size = 32;
+const Block = [block_size * block_size]f32;
 
 pub const Matrix = struct {
     rows: usize,
@@ -49,67 +51,58 @@ pub const Matrix = struct {
 
     pub fn setValue(self: *Matrix, row: usize, col: usize, value: f32) void {
         if (row < self.rows and col < self.cols) {
-            self.data[self.rows * row + col] = value;
+            self.data[(self.cols * row) + col] = value;
         }
     }
 
-    pub fn transpose(self: *Matrix) void {
+    pub fn reshape(self: *Matrix, rows: usize, cols: usize) !void {
+        if (self.rows * self.cols != rows * cols) {
+            return MatrixError.InvalidShape;
+        }
+
+        self.rows = rows;
+        self.cols = cols;
+    }
+
+    pub fn transpose(self: *Matrix) !Matrix {
         const m = self.rows;
         const n = self.cols;
 
-        const num_m_blocks = (m + block_size - 1) / block_size;
-        const num_n_blocks = (n + block_size - 1) / block_size;
+        var result = try Matrix.init(self.allocator, n, m);
 
-        for (0..num_m_blocks) |m_block| {
-            const m_block_size = @min((m_block + 1) * block_size, m) - m_block * block_size;
-            for (m_block..num_n_blocks - 1) |n_block| {
-                // Transpose top right block
-                for (0..m_block_size) |i| {
-                    for (i..block_size) |j| {
-                        const m_index = (m_block * block_size + i) * n + n_block * block_size + j;
-                        const n_index = (n_block * block_size + j) * m + m_block * block_size + i;
-
-                        const temp = self.data[m_index];
-                        self.data[m_index] = self.data[n_index];
-                        self.data[n_index] = temp;
-                    }
-                }
-            }
-
-            // transpose bottom left blocks
-            const n_block = num_n_blocks - 1;
-            const n_block_size = @min(num_n_blocks * block_size, n) - (num_n_blocks - 1) * block_size;
-            for (0..n_block_size) |j| {
-                for (j..m_block_size) |i| {
-                    const m_index = (m_block * block_size + i) * n + n_block * block_size + j;
-                    const n_index = (n_block * block_size + j) * m + m_block * block_size + i;
-
-                    const temp = self.data[m_index];
-                    self.data[m_index] = self.data[n_index];
-                    self.data[n_index] = temp;
-                }
+        for (0..m) |i| {
+            for (0..n) |j| {
+                result.data[j * m + i] = self.data[i * n + j];
             }
         }
 
-        self.rows = n;
-        self.cols = m;
+        return result;
     }
 
     test "transpose" {
         const allocator = std.testing.allocator;
 
-        const n: usize = 4;
-        var actual = try Matrix.init(allocator, n, n);
-        defer actual.deinit();
+        const m: usize = 7;
+        const n: usize = 6;
 
-        var expected = try Matrix.init(allocator, n, n);
+        var initial = try Matrix.init(allocator, m, n);
+        defer initial.deinit();
+
+        var expected = try Matrix.init(allocator, n, m);
         defer expected.deinit();
 
         var count: f32 = 1;
-        for (0..n) |i| {
+        for (0..m) |i| {
             for (0..n) |j| {
-                actual.setValue(i, j, count);
-                expected.setValue(j, i, count);
+                initial.setValue(i, j, count);
+                count += 1;
+            }
+        }
+
+        count = 1;
+        for (0..m) |j| {
+            for (0..n) |i| {
+                expected.setValue(i, j, count);
                 count += 1;
             }
         }
@@ -117,17 +110,21 @@ pub const Matrix = struct {
         // time transpose
         var timer = try std.time.Timer.start();
 
-        actual.transpose();
+        const actual = try initial.transpose();
+        defer actual.deinit();
 
         const duration = timer.read();
 
-        std.debug.print("\nMatrix transpose of a {}x{} matrices took {} ns.\n", .{ n, n, duration });
+        std.debug.print("\nMatrix transpose of a {}x{} matrix took {} ns.\n", .{ m, n, duration });
 
         // verify
+        try std.testing.expectEqual(n, actual.rows);
+        try std.testing.expectEqual(m, actual.cols);
+
         const tol = std.math.floatEps(f32);
         for (0..n) |i| {
-            for (0..n) |j| {
-                try std.testing.expectApproxEqRel(expected.data[i * n + j], actual.data[i * n + j], tol);
+            for (0..m) |j| {
+                try std.testing.expectApproxEqRel(expected.data[i * m + j], actual.data[i * m + j], tol);
             }
         }
     }
@@ -137,161 +134,174 @@ pub const Matrix = struct {
             return MatrixError.NonMatchingDims;
         }
 
-        var result = try Matrix.init(self.allocator, self.rows, other.cols);
-
-        try blockGemm(self, other, &result);
-
-        return result;
-    }
-
-    test matmul {
-        const allocator = std.testing.allocator;
-
-        const n: usize = 4;
-        var A = try Matrix.init(allocator, n, n);
-        defer A.deinit();
-
-        var B = try Matrix.init(allocator, n, n);
-        defer B.deinit();
-
-        var expected = try Matrix.init(allocator, n, n);
-        defer expected.deinit();
-
-        var base_val: f32 = 0;
-        for (0..n) |i| {
-            base_val += @floatFromInt(i + 1);
-        }
-
-        var count: f32 = 1;
-        for (0..n) |i| {
-            for (0..n) |j| {
-                A.setValue(i, j, count);
-                B.setValue(i, j, 1);
-
-                const i_f32: f32 = @floatFromInt(i);
-
-                expected.setValue(i, j, base_val + i_f32 * n * n);
-
-                count += 1;
-            }
-        }
-
-        // time gemm
-        var timer = try std.time.Timer.start();
-
-        const actual: Matrix = try A.matmul(&B);
-        defer actual.deinit();
-
-        const duration = timer.read();
-
-        std.debug.print("\nMatrix multiplication of two {}x{} matrices took {} ns.\n", .{ n, n, duration });
-
-        // verify
-        const tol = std.math.floatEps(f32) * (n + 1);
-        for (0..n) |i| {
-            for (0..n) |j| {
-                try std.testing.expectApproxEqRel(expected.data[i * n + j], actual.data[i * n + j], tol);
-            }
-        }
-    }
-
-    fn gemm(self: *Matrix, other: *Matrix, result: *Matrix) void {
-        other.transpose();
-
-        var sum: f32 = 0;
-        for (0..self.rows) |i| {
-            for (0..other.cols) |j| {
-                sum = 0;
-                for (0..other.rows) |k| {
-                    sum += self.data[self.rows * i + k] * other.data[other.rows * j + k];
-                }
-                result.data[self.rows * i + j] = sum;
-            }
-        }
-
-        other.transpose();
-    }
-
-    fn blockGemm(self: *Matrix, other: *Matrix, result: *Matrix) !void {
-        const blk_f32 = @Vector(block_size, f32);
-
         const m = self.rows;
         const n = other.rows;
         const p = other.cols;
+
+        var result = try Matrix.init(self.allocator, m, p);
 
         const num_m_blocks = (m + block_size - 1) / block_size;
         const num_n_blocks = (n + block_size - 1) / block_size;
         const num_p_blocks = (p + block_size - 1) / block_size;
 
-        var one_block: [block_size * block_size]f32 = undefined;
-        var two_block: [block_size * block_size]f32 = undefined;
-        var thr_block: [block_size * block_size]f32 = undefined;
+        var block_A: [block_size * block_size]f32 = undefined;
+        var block_B: [block_size * block_size]f32 = undefined;
+        var block_C: [block_size * block_size]f32 = undefined;
 
         for (0..num_m_blocks) |m_idx| {
             for (0..num_p_blocks) |p_idx| {
                 for (0..num_n_blocks) |n_idx| {
-                    // clear local blocks
-                    for (0..block_size) |i| {
-                        for (0..block_size) |j| {
-                            const inner_block_index = i * block_size + j;
+                    // set block for A
+                    blockA(self, m_idx, n_idx, &block_A);
 
-                            one_block[inner_block_index] = 0;
-                            two_block[inner_block_index] = 0;
-                            thr_block[inner_block_index] = 0;
-                        }
-                    }
-
-                    // set local blocks
-                    const self_block_width = @min(n_idx * block_size + block_size, self.cols) - (n_idx * block_size);
-                    const self_block_height = @min(m_idx * block_size + block_size, self.rows) - (m_idx * block_size);
-                    const self_block_idx = m_idx * block_size * self.cols + n_idx * block_size;
-
-                    for (0..self_block_height) |i| {
-                        for (0..self_block_width) |j| {
-                            const inner_block_index = i * block_size + j;
-                            const self_idx = self_block_idx + i * self.cols + j;
-
-                            one_block[inner_block_index] = self.data[self_idx];
-                        }
-                    }
-
-                    const other_block_width = @min(n_idx * block_size + block_size, n) - (n_idx * block_size);
-                    const other_block_height = @min(p_idx * block_size + block_size, p) - (p_idx * block_size);
-                    const other_block_idx = p_idx * block_size * n + n_idx * block_size;
-
-                    for (0..other_block_height) |i| {
-                        for (0..other_block_width) |j| {
-                            const inner_block_index = i * block_size + j;
-                            const other_idx = other_block_idx + i * other.cols + j;
-
-                            two_block[inner_block_index] = other.data[other_idx];
-                        }
-                    }
+                    // set block for B
+                    blockB(other, n_idx, p_idx, &block_B);
 
                     // perform calc
-                    for (0..block_size) |i| {
-                        for (0..block_size) |j| {
-                            const one_vec: blk_f32 = (&one_block)[i * block_size ..][0..block_size].*;
-                            const two_vec: blk_f32 = (&two_block)[j * block_size ..][0..block_size].*;
+                    multiplyBlocks(&block_A, &block_B, &block_C);
 
-                            const sum_vec = one_vec * two_vec;
-
-                            thr_block[i * block_size + j] = @reduce(.Add, sum_vec);
-                        }
-                    }
-
-                    // copy thr_block to result
-                    const result_block_width = @min(p_idx * block_size + block_size, n) - (p_idx * block_size);
-                    const result_block_height = @min(m_idx * block_size + block_size, m) - (m_idx * block_size);
-                    const result_block_idx = m_idx * block_size * n + p_idx * block_size;
-
-                    for (0..result_block_height) |i| {
-                        for (0..result_block_width) |j| {
-                            const result_idx = result_block_idx + i * result.cols + j;
-                            result.data[result_idx] += thr_block[i * block_size + j];
-                        }
-                    }
+                    // copy block_C to result
+                    copyBlockToMatrix(&result, m_idx, p_idx, &block_C);
                 }
+            }
+        }
+
+        return result;
+    }
+
+    inline fn copyBlockToMatrix(matrix: *Matrix, block_i: usize, block_j: usize, block: *Block) void {
+        const m = matrix.rows;
+        const p = matrix.cols;
+
+        const block_height = @min(block_i * block_size + block_size, m) - (block_i * block_size);
+        const block_width = @min(block_j * block_size + block_size, p) - (block_j * block_size);
+        const block_idx = block_i * block_size * p + block_j * block_size;
+
+        for (0..block_height) |i| {
+            for (0..block_width) |j| {
+                const idx = block_idx + i * p + j;
+                matrix.data[idx] += block[i * block_size + j];
+            }
+        }
+    }
+
+    inline fn multiplyBlocks(block_A: *Block, block_B: *Block, block_C: *Block) void {
+        for (0..block_size) |i| {
+            for (0..block_size) |j| {
+                const vec_A: @Vector(block_size, f32) = block_A[i * block_size ..][0..block_size].*;
+                const vec_B: @Vector(block_size, f32) = block_B[j * block_size ..][0..block_size].*;
+
+                const sum_vec = vec_A * vec_B;
+
+                block_C[i * block_size + j] = @reduce(.Add, sum_vec);
+            }
+        }
+    }
+
+    inline fn blockA(matrix: *Matrix, block_i: usize, block_j: usize, block: *[block_size * block_size]f32) void {
+        const m = matrix.rows;
+        const n = matrix.cols;
+
+        const block_height = @min(block_i * block_size + block_size, m) - (block_i * block_size);
+        const block_width = @min(block_j * block_size + block_size, n) - (block_j * block_size);
+        const block_idx = block_i * block_size * n + block_j * block_size;
+
+        for (0..block_height) |i| {
+            for (0..block_width) |j| {
+                const inner_block_index = i * block_size + j;
+                const idx = block_idx + i * n + j;
+
+                block[inner_block_index] = matrix.data[idx];
+            }
+            // zero padding
+            for (block_width..block_size) |j| {
+                block[i * block_size + j] = 0;
+            }
+        }
+
+        // zero padding
+        for (block_height..block_size) |i| {
+            for (0..block_size) |j| {
+                block[i * block_size + j] = 0;
+            }
+        }
+    }
+
+    inline fn blockB(matrix: *Matrix, block_i: usize, block_j: usize, block: *[block_size * block_size]f32) void {
+        const m = matrix.rows;
+        const n = matrix.cols;
+
+        const block_height = @min(block_i * block_size + block_size, m) - (block_i * block_size);
+        const block_width = @min(block_j * block_size + block_size, n) - (block_j * block_size);
+        const block_idx = block_i * block_size * n + block_j * block_size;
+
+        for (0..block_height) |i| {
+            for (0..block_width) |j| {
+                const inner_block_index = j * block_size + i;
+                const other_idx = block_idx + i * n + j;
+
+                block[inner_block_index] = matrix.data[other_idx];
+            }
+            // zero padding
+            for (block_width..block_size) |j| {
+                block[j * block_size + i] = 0;
+            }
+        }
+
+        // zero padding
+        for (block_height..block_size) |i| {
+            for (0..block_size) |j| {
+                block[j * block_size + i] = 0;
+            }
+        }
+    }
+
+    test "matmul" {
+        const allocator = std.testing.allocator;
+
+        const m: usize = 50;
+        const n: usize = 60;
+        const p: usize = 70;
+
+        // define A: matrix going from 1 to m*n
+        var A = try Matrix.init(allocator, m, n);
+        defer A.deinit();
+
+        var count: f32 = 1;
+        for (0..m) |i| {
+            for (0..n) |j| {
+                A.setValue(i, j, count);
+                count += 1;
+            }
+        }
+
+        // define B: matrix of 1s
+        var B = try Matrix.init(allocator, n, p);
+        defer B.deinit();
+
+        for (0..n) |i| {
+            for (0..p) |j| {
+                B.setValue(i, j, 1);
+            }
+        }
+
+        // timer
+        var timer = try std.time.Timer.start();
+
+        const C = try A.matmul(&B);
+        defer C.deinit();
+
+        const duration = timer.read();
+
+        std.debug.print("\nMatrix multiplication of a {}x{} matrix and a {}x{} matrix took {} ns.\n", .{ m, n, n, p, duration });
+
+        // test
+        const base_val: f32 = (n * (n + 1)) / 2;
+        const tol = std.math.floatEps(f32) * (n + 1);
+        for (0..m) |i| {
+            const i_f32: f32 = @floatFromInt(i);
+            for (0..p) |j| {
+                try std.testing.expectApproxEqRel(base_val + i_f32 * n * n, C.data[i * p + j], tol);
             }
         }
     }
